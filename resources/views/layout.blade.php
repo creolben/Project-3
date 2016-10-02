9a2cb599f802a4cc0ab515720cbc2b6e978e27d1
Ben Jerome <benjaminjjerome@gmail.com>
2016-10-02 12:48:16 -0400

    added page and layout files

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,61 +1,40 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Project 3</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="style.css" type="text/css">
-	<link href="css/lightbox.css" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Poppins:400,600,700,500,300' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,400italic,500,500italic,300,100italic,100,300italic' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-<header class="header">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4 ">
-				<div class="navbar-header">
-					    <button type="button" class="navbar-toggle menu-button" data-toggle="collapse" data-target="#myNavbar">
-					        <span class="glyphicon glyphicon-align-justify"></span>
-					    </button>
-      					<a class="navbar-brand logo" href="#">Project 3</a>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="style.css" type="text/css">
+<link href="css/lightbox.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Poppins:400,600,700,500,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,900,700italic,700,400italic,500,500italic,300,100italic,100,300italic' rel='stylesheet' type='text/css'>
+  @extends('header')
+  <body>
+    <header class="header">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-4 ">
+    				<div class="navbar-header">
+    					    <button type="button" class="navbar-toggle menu-button" data-toggle="collapse" data-target="#myNavbar">
+    					        <span class="glyphicon glyphicon-align-justify"></span>
+    					    </button>
+          					<a class="navbar-brand logo" href="#">Project 3</a>
+        			</div>
     			</div>
-			</div>
-			<div class="col-md-8">
-				<nav class="collapse navbar-collapse" id="myNavbar" role="navigation">
-					<ul class="nav navbar-nav navbar-right menu">
-							<li><a href="#page-top" class="page-scroll active">Home</a></li>
-							<li><a href="#section2" class="page-scroll">Features</a></li>
-							<li><a href="#work" class="page-scroll">Portfolio</a></li>
-							<li><a href="#section4" class="page-scroll">Contact</a></li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-	</div>
-</header>
-
-<div class="container-fluid main" id="page-top">
-	   @yield('content')
-</div>
-
-<div class="container-fluid footer">
-	<div class="row">
-		<div class="col-md-12">
-			<p>Copyright &copy; Ravalic 2015 By <a href="http://www.html5layouts.com">HTML5 Layouts</a> | <a href="http://vectortoons.com/">Get Vector Graphics</a></p>
-		</div>
-	</div>
-</div>
-
+    			<div class="col-md-8">
+    				<nav class="collapse navbar-collapse" id="myNavbar" role="navigation">
+    					<ul class="nav navbar-nav navbar-right menu">
+    							<li><a href="#page-top" class="page-scroll active">Home</a></li>
+    							<li><a href="#section2" class="page-scroll">Features</a></li>
+    							<li><a href="#work" class="page-scroll">Portfolio</a></li>
+    							<li><a href="#section4" class="page-scroll">Contact</a></li>
+    					</ul>
+    				</nav>
+    			</div>
+    		</div>
+    	</div>
+    </header>
+    <div class="container-fluid main" id="page-top">
+    	   @yield('content')
+    </div>
+      @extends('footer')
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -77,45 +56,45 @@
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>
     <script>
-	$(document).ready(function () {
-		$(document).on("scroll", onScroll);
+    $(document).ready(function () {
+    $(document).on("scroll", onScroll);
 
-		$('a[href^="#"]').on('click', function (e) {
-			e.preventDefault();
-			$(document).off("scroll");
+    $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
 
-			$('a').each(function () {
-				$(this).removeClass('active');
-			})
-			$(this).addClass('active');
+      $('a').each(function () {
+        $(this).removeClass('active');
+      })
+      $(this).addClass('active');
 
-			var target = this.hash;
-			$target = $(target);
-			$('html, body').stop().animate({
-				'scrollTop': $target.offset().top
-			}, 500, 'swing', function () {
-				window.location.hash = target;
-				$(document).on("scroll", onScroll);
-			});
-		});
-	});
+      var target = this.hash;
+      $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+      });
+    });
+    });
 
-	function onScroll(event){
-		var scrollPosition = $(document).scrollTop();
-		$('nav a').each(function () {
-			var currentLink = $(this);
-			var refElement = $(currentLink.attr("href"));
-			if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-				$('nav ul li a').removeClass("active");
-				currentLink.addClass("active");
-			}
-			else{
-				currentLink.removeClass("active");
-			}
-		});
-	}
-	</script>
-	<script type="text/javascript">
+    function onScroll(event){
+    var scrollPosition = $(document).scrollTop();
+    $('nav a').each(function () {
+      var currentLink = $(this);
+      var refElement = $(currentLink.attr("href"));
+      if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+        $('nav ul li a').removeClass("active");
+        currentLink.addClass("active");
+      }
+      else{
+        currentLink.removeClass("active");
+      }
+    });
+    }
+    </script>
+    <script type="text/javascript">
     jQuery(function ($) {
       // custom formatting example
       $('.timer').data('countToOptions', {
@@ -127,7 +106,7 @@
       // start all the timers
       $('#starts').waypoint(function() {
     $('.timer').each(count);
-	});
+    });
 
       function count(options) {
         var $this = $(this);
@@ -135,6 +114,6 @@
         $this.countTo(options);
       }
     });
-  	</script>
-</body>
+    </script>
+  </body>
 </html>
